3ac8ecf6b570166605493b214b09a9b63a4ea104
“ljs” <1060545627@qq.com>
2017-11-16 08:04:11 +0800

    图片居中缩放

--- src/com/ljs/customview/view/PhotoZoomView.java
+++ src/com/ljs/customview/view/PhotoZoomView.java
@@ -4,16 +4,25 @@ import android.R.integer;
 import android.annotation.SuppressLint;
 import android.content.Context;
 import android.graphics.Matrix;
+import android.graphics.RectF;
 import android.graphics.drawable.Drawable;
 import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.ScaleGestureDetector;
+import android.view.ScaleGestureDetector.OnScaleGestureListener;
+import android.view.View;
 import android.view.ViewTreeObserver.OnGlobalLayoutListener;
 import android.widget.ImageView;
+import android.view.View.OnTouchListener;
+
 /**
  * 自由缩放移动图片控件
+ * 
  * @author ljs
- *
+ * 
  */
-public class PhotoZoomView extends ImageView implements OnGlobalLayoutListener{
+public class PhotoZoomView extends ImageView implements OnGlobalLayoutListener,
+		OnScaleGestureListener, OnTouchListener {
 
 	public PhotoZoomView(Context context) {
 		this(context, null);
@@ -25,32 +34,36 @@ public class PhotoZoomView extends ImageView implements OnGlobalLayoutListener{
 
 	public PhotoZoomView(Context context, AttributeSet attrs, int defStyle) {
 		super(context, attrs, defStyle);
-		init();
+		init(context);
 	}
 
 	private boolean monce = false;
-	
-	private float mInitScale ;
-	
+
+	private float mInitScale;
+
 	private float mMidScale;
-	
+
 	private float mMaxScale;
-	
+
 	private Matrix matrix;
-	
-	
-	private void init() {
+
+	private ScaleGestureDetector mScaleGestureDetector;
+
+	private void init(Context context) {
 		matrix = new Matrix();
+		setScaleType(ScaleType.MATRIX);
+		mScaleGestureDetector = new ScaleGestureDetector(context, this);
+		setOnTouchListener(this);
 	}
-	
-	
+
 	@Override
 	protected void onAttachedToWindow() {
 		super.onAttachedToWindow();
 		getViewTreeObserver().addOnGlobalLayoutListener(this);
 	}
-	
-	@SuppressLint("NewApi") @Override
+
+	@SuppressLint("NewApi")
+	@Override
 	protected void onDetachedFromWindow() {
 		super.onDetachedFromWindow();
 		getViewTreeObserver().removeOnGlobalLayoutListener(this);
@@ -61,44 +74,146 @@ public class PhotoZoomView extends ImageView implements OnGlobalLayoutListener{
 	 */
 	@Override
 	public void onGlobalLayout() {
-		if(!monce){
-			
+		if (!monce) {
+
 			int width = getWidth();
 			int height = getHeight();
-			
+
 			Drawable d = getDrawable();
-			
-			if (d==null) {
+
+			if (d == null) {
 				return;
-				
+
 			}
 			int dw = d.getIntrinsicWidth();
 			int dh = d.getIntrinsicHeight();
-			
+
 			float scale = 1.0f;
-			
-			
-			if(dw>width&&dh<height){
-				scale = width*1.0f/dw;
-			}else if(dh>height&&dw<width){
-				scale  = height*1.0f/dh;
-			}else if((dw>width&&dh>height) ||(dw<width&&dh<height)){
-				scale = Math.min(width*1.0f/dw, height*1.0f/dh);
+
+			if (dw > width && dh < height) {
+				scale = width * 1.0f / dw;
+			} else if (dh > height && dw < width) {
+				scale = height * 1.0f / dh;
+			} else if ((dw > width && dh > height)
+					|| (dw < width && dh < height)) {
+				scale = Math.min(width * 1.0f / dw, height * 1.0f / dh);
 			}
 			mInitScale = scale;
 			mMidScale = scale * 2;
-			mMaxScale = scale *4;
-			
-			
-			int dx  = getWidth()/2 -dw/2;
-			int dy = getHeight()/2 - dh/2;
-			
+			mMaxScale = scale * 4;
+
+			int dx = getWidth() / 2 - dw / 2;
+			int dy = getHeight() / 2 - dh / 2;
+
 			matrix.postTranslate(dx, dy);
-			matrix.postScale(mInitScale, mInitScale, width/2, height/2);
+			matrix.postScale(mInitScale, mInitScale, width / 2, height / 2);
 			setImageMatrix(matrix);
-			
+
 			monce = true;
 		}
 	}
-	
+
+	private float getScale() {
+		float[] values = new float[9];
+		matrix.getValues(values);
+		return values[Matrix.MSCALE_X];
+	}
+
+	@Override
+	public boolean onScale(ScaleGestureDetector detector) {
+
+		float scale = getScale();
+
+		float scaleFactor = detector.getScaleFactor();
+
+		if (getDrawable() == null)
+			return true;
+
+		if ((scale < mMaxScale && scaleFactor > 1.0f)
+				|| (scale > mInitScale && scaleFactor < 1.0f)) {
+			if (scale * scaleFactor < mInitScale) {
+				scaleFactor = mInitScale / scale;
+			}
+			if (scale * scaleFactor > mMaxScale) {
+				scale = mMaxScale / scale;
+			}
+
+			matrix.postScale(scaleFactor, scaleFactor, detector.getFocusX(),
+					detector.getFocusY());
+			checkBorderAndCenterWhenScale();
+			setImageMatrix(matrix);
+		}
+
+		return true;
+	}
+
+	private RectF getMatrixRectF() {
+		Matrix matrix = this.matrix;
+		RectF rectF = new RectF();
+
+		Drawable d = getDrawable();
+		if (d != null) {
+			rectF.set(0, 0, d.getIntrinsicWidth(), d.getIntrinsicHeight());
+			matrix.mapRect(rectF);
+		}
+
+		return rectF;
+	}
+
+	private void checkBorderAndCenterWhenScale() {
+		RectF rectF = getMatrixRectF();
+
+		float deltaX = 0;
+		float deltaY = 0;
+
+		float width = getWidth();
+		float height = getHeight();
+
+		if (rectF.width() >= width) {
+			if (rectF.left > 0) {
+				deltaX = -rectF.left;
+			}
+			if (rectF.right < width) {
+				deltaX = width - rectF.right;
+			}
+		}
+		if(rectF.height()>=height){
+			if (rectF.top>0) {
+			deltaY = -rectF.top;	
+			}
+			if (rectF.bottom<height) {
+				deltaY = height - rectF.bottom;
+			}
+			
+		}
+		
+		
+		if (rectF.width()< width) {
+			deltaX = width/2 -rectF.right+rectF.width()/2;
+		}
+		if (rectF.height()<height) {
+			deltaY = height/2-rectF.bottom+rectF.height()/2;
+		}
+		
+		matrix.postTranslate(deltaX, deltaY);
+		
+		
+	}
+
+	@Override
+	public boolean onScaleBegin(ScaleGestureDetector detector) {
+		return true;
+	}
+
+	@Override
+	public void onScaleEnd(ScaleGestureDetector detector) {
+
+	}
+
+	@Override
+	public boolean onTouch(View v, MotionEvent event) {
+		mScaleGestureDetector.onTouchEvent(event);
+		return true;
+	}
+
 }
