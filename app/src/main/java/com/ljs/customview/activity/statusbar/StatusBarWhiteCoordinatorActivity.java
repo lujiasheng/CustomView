f0b80136d33d654849ffb2671c2e3e0dd9532be4
ljs <1060545627@qq.com>
2018-06-13 14:40:53 +0800

    增加SDK版本统一控制

--- app/src/main/java/com/ljs/customview/activity/statusbar/StatusBarWhiteCoordinatorActivity.java
+++ app/src/main/java/com/ljs/customview/activity/statusbar/StatusBarWhiteCoordinatorActivity.java
@@ -1,13 +1,11 @@
 package com.ljs.customview.activity.statusbar;
 
 
-import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.AppBarLayout;
 import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v4.content.ContextCompat;
-import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
 import com.ljs.customview.R;
@@ -22,13 +20,13 @@ public class StatusBarWhiteCoordinatorActivity extends BaseActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_statusbar_white_coordinator);
-        Toolbar toolbar = findViewById(R.id.toolbar);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         toolbar.setNavigationIcon(R.drawable.ic_arrow_back);
         setSupportActionBar(toolbar);
         getSupportActionBar().setTitle("StatusBar White whith CoordinatorLayout");
 
-        AppBarLayout mAppBarLayout = findViewById(R.id.appbar);
-        CollapsingToolbarLayout collapsingToolbarLayout = findViewById(R.id.collapsing_layout);
+        AppBarLayout mAppBarLayout = (AppBarLayout) findViewById(R.id.appbar);
+        CollapsingToolbarLayout collapsingToolbarLayout = (CollapsingToolbarLayout) findViewById(R.id.collapsing_layout);
         collapsingToolbarLayout.setTitle(getString(R.string.app_name));
 
         StatusBarUtils.setStatusBarLightForCollapsingToolbar(this, mAppBarLayout, collapsingToolbarLayout, toolbar, ContextCompat.getColor(this, R.color.colorPrimary));
